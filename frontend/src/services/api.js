9d979a665d655783ce92185f245490f0d0aa40b9
nicolaslima-dev <nicolassilvap1209@gmail.com>
2026-08-05 12:36:06 -0300

    fix: teste sem a utl do localhost

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+export const BASE_URL = 'https://trabalho-corretora-backend.onrender.com';
 
 console.log('🚨 API BASE URL ATUAL:', BASE_URL);
-
 const api = axios.create({
     baseURL: BASE_URL,
 });
